c20aea5ace61d649eb80a0d054d8612e9c0fe400
Victor <vsuarez@melkartconsulting.com>
2017-06-20 10:26:44 +0200

    home

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,7 @@
 Route::auth();
 
 // Route::get('/',['middleware' => 'authSubAdmin','middleware' => 'authRole','uses' => 'Admin\BackendController@index']);
+Route::get('/','HomeController@index');
 
 
 // Route::get('/admin/propietario',['middleware' => 'authSubAdmin','uses' => 'Admin\BackendController@index']);
