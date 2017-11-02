75c20ccc11cdd1bce7935b678491c194a39f9f2c
Victor <vsuarez@melkartconsulting.com>
2017-11-02 08:44:06 +0100

    jesus

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,24 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<link href="//fonts.googleapis.com/css?family=Open+Sans%3A800|Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700,800,900|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		
+		<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('/img/miramarski/favicon/apple-icon-57x57.png') }}">
+		<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('/img/miramarski/favicon/apple-icon-60x60.png') }}">
+		<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/img/miramarski/favicon/apple-icon-72x72.png') }}">
+		<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/img/miramarski/favicon/apple-icon-76x76.png') }}">
+		<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/img/miramarski/favicon/apple-icon-114x114.png') }}">
+		<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('/img/miramarski/favicon/apple-icon-120x120.png') }}">
+		<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/img/miramarski/favicon/apple-icon-144x144.png') }}">
+		<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('/img/miramarski/favicon/apple-icon-152x152.png') }}">
+		<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/img/miramarski/favicon/apple-icon-180x180.png') }}">
+		<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('/img/miramarski/favicon/android-icon-192x192.png') }}">
+		<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/img/miramarski/favicon/favicon-32x32.png') }}">
+		<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/img/miramarski/favicon/favicon-96x96.png') }}">
+		<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/miramarski/favicon/favicon-16x16.png') }}">
+		<link rel="manifest" href="/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="{{ asset('/img/miramarski/favicon/ms-icon-144x144.png' ) }}">
+		<meta name="theme-color" content="#ffffff">
 
  
 			<link rel="stylesheet" href="{{ asset ('/css/app.css')}}" type="text/css" />
@@ -33,6 +51,16 @@
 
 		  gtag('config', 'UA-66225892-1');
 		</script>
+		
+		<script type="text/javascript">
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-66225892-1', 'auto'); 
+		  ga('send', 'pageview'); 
+		</script>
+
 
 		<title>@yield('title')</title>
 		
