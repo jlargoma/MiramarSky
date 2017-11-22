4807a2480f3ffa0668b0c64a47c453877c5350c1
Ian Avila <iankurosaki17@gmail.com>
2017-11-22 08:07:43 +0100

    hg

--- config/app.php
+++ config/app.php
@@ -149,7 +149,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -207,7 +206,6 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'PDF'       => Barryvdh\DomPDF\Facade::class,
         'Excel'     => Maatwebsite\Excel\Facades\Excel::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
