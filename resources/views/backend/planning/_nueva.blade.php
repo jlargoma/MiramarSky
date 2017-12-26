f72c82938364ec9e8c054b60f57ae750dd16c647
Francisco Sanz <fsanz@daimonconsulting.com>
2017-12-26 14:01:43 +0100

    ad

--- resources/views/backend/planning/_nueva.blade.php
+++ resources/views/backend/planning/_nueva.blade.php
@@ -68,7 +68,7 @@
                 </div>
                 <div class="col-md-4 col-xs-12 push-10">
                     <label for="phone">Telefono</label> 
-                    <input class="form-control cliente" type="number" name="phone" >
+                    <input class="form-control cliente only-numbers" type="text" name="phone" >
                 </div>  
                 <div class="col-md-3 col-xs-12 push-10">
                     <label for="dni">DNI</label> 
@@ -591,6 +591,26 @@
             } else {
                 $('.content-cities').show();
 
+            }
+        });
+
+        $('.only-numbers').keydown(function (e) {
+            // Allow: backspace, delete, tab, escape, enter and .
+            if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190, 32, 107 ]) !== -1 ||
+                 // Allow: Ctrl/cmd+A
+                (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: Ctrl/cmd+C
+                (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: Ctrl/cmd+X
+                (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: home, end, left, right
+                (e.keyCode >= 35 && e.keyCode <= 39)) {
+                     // let it happen, don't do anything
+                     return;
+            }
+            // Ensure that it is a number and stop the keypress
+            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                e.preventDefault();
             }
         });
     });
--- resources/views/backend/planning/update.blade.php
+++ resources/views/backend/planning/update.blade.php
@@ -619,7 +619,7 @@
                          <?php endif ?> 
                         <div class="col-xs-12 push-10">
                             <label for="phone">Telefono</label> 
-                            <input class="form-control cliente" type="text" name="phone" value="<?php echo $book->customer->phone ?>" data-id="<?php echo $book->customer->id ?>"> 
+                            <input class="form-control only-numbers cliente" type="text" name="phone" value="<?php echo $book->customer->phone ?>" data-id="<?php echo $book->customer->id ?>"> 
                         </div>  
                         <div class="col-xs-12 push-10">
                             <label for="dni">DNI</label> 
@@ -1445,6 +1445,26 @@
             } else {
                 $('.content-cities').show();
 
+            }
+        });
+
+        $('.only-numbers').keydown(function (e) {
+            // Allow: backspace, delete, tab, escape, enter and .
+            if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190, 32, 107 ]) !== -1 ||
+                 // Allow: Ctrl/cmd+A
+                (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: Ctrl/cmd+C
+                (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: Ctrl/cmd+X
+                (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+                 // Allow: home, end, left, right
+                (e.keyCode >= 35 && e.keyCode <= 39)) {
+                     // let it happen, don't do anything
+                     return;
+            }
+            // Ensure that it is a number and stop the keypress
+            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                e.preventDefault();
             }
         });
 
