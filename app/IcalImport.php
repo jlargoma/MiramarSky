17b53bbc668c505c53beb0d803056a532876097f
Ian Avila <iankurosaki17@gmail.com>
2018-01-30 00:54:08 +0100

    migration and model

--- app/IcalImport.php
+++ app/IcalImport.php
@@ -6,31 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class IcalImport extends Model
 {
-    protected $kink    = 0;
-    protected $room_id = 0;
-
-
-    public function getLink()
-    {
-    	return $this->link;
-    }
-
-    public function setLink($link)
-    {
-    	$this->link = $link;
-    	return $this->link;
-    }
-
-    public function getRoom()
-    {
-    	return $this->room_id;
-    }
-
-    public function setRoom($room_id)
-    {
-    	$this->room_id = $room_id;
-    	return $this->room_id;
-    }
+   protected $table = "ical_import";
 
     
 }
