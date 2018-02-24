5c08747248c83dd822473f9c57acf91a414c79ba
Ian Avila <iankurosaki17@gmail.com>
2018-02-24 16:03:35 +0100

    add invoices

---
+++ database/migrations/2018_02_24_150115_create_invoices.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('book_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
+            $table->date('date');
+            $table->string('phone');
+            $table->string('postalcode');
+            $table->string('nif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
