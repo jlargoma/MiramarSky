9aa8eb967426d75124838df36295e15dbe2f6a39
Ian Avila <iankurosaki17@gmail.com>
2018-09-08 22:18:04 +0200

    changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -229,12 +229,6 @@
 <script type="text/javascript" src="{{ asset('/frontend/js/plugins.js')}}"></script>
 <script type="text/javascript" src="{{ asset('/frontend/js/functions.js')}}"></script>
 
-<script type="text/javascript" src="{{ asset('/js/flip.js')}}"></script>
-
-<script type="text/javascript" src="{{asset('/frontend/js/components/moment.js')}}"></script>
-<script type="text/javascript" src="{{asset('/frontend/js/components/daterangepicker.js')}}"></script>
-<script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
-
 {{--<script type="text/javascript" src="{{ asset('/js/scripts-slider.js')}}"></script>--}}
 <script type="text/javascript" src="{{ asset('/frontend/include/rs-plugin/js/jquery.themepunch.tools.min.js')}}"></script>
 <script type="text/javascript" src="{{ asset('/frontend/include/rs-plugin/js/jquery.themepunch.revolution.min.js')}}"></script>
@@ -253,6 +247,12 @@
 <script type="text/javascript" src="{{ asset('/frontend/include/rs-plugin/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
 <script type="text/javascript" src="{{ asset('/frontend/include/rs-plugin/js/extensions/revolution.extension.video.min.js')}}"></script>
 
+
+<script type="text/javascript" src="{{ asset('/js/flip.js')}}"></script>
+
+<script type="text/javascript" src="{{asset('/frontend/js/components/moment.js')}}"></script>
+<script type="text/javascript" src="{{asset('/frontend/js/components/daterangepicker.js')}}"></script>
+<script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
 <?php /* view para todos los scripts generales de la pagina*/ ?>
 @include('layouts._generalScripts')
 
