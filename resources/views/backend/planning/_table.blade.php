e847e1562f427b6e357d3ce47a6fd0845176f5df
Ian Avila <iankurosaki17@gmail.com>
2018-12-04 11:25:59 +0100

    fix bug order

--- resources/views/backend/planning/_table.blade.php
+++ resources/views/backend/planning/_table.blade.php
@@ -15,7 +15,7 @@
 		<script>
 		  $('.table-data').dataTable({
 			"searching": false,
-			"order": [[ 5, "desc" ]],
+			"order": [ 5, "asc" ],
 			"paging":  false,
 			"columnDefs": [
 			  { "targets": [0,1,2,3,4,9,10,11,12], "orderable": false }
@@ -27,7 +27,7 @@
 		<script>
 		  $('.table-data').dataTable({
 			"searching": false,
-			"order": [[ 5, "desc" ]],
+			"order": [ 5, "asc" ],
 			"paging":   false,
 			"columnDefs": [
 			  {"targets": [0,3,4,6,7,8,9,10], "orderable": false }
