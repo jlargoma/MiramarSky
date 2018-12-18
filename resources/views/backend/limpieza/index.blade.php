eb9335aeee7c0ecdaf89b1de4047f664481d757b
Ian Avila <iankurosaki17@gmail.com>
2018-12-18 12:22:57 +0100

    mobile limpieza

--- resources/views/backend/limpieza/index.blade.php
+++ resources/views/backend/limpieza/index.blade.php
@@ -18,36 +18,37 @@ setlocale(LC_TIME, "es_ES");
     </style>
 @endsection
 @section('content')
-    <?php if (!$mobile->isMobile() ): ?>
     <div class="container  p-l-15 p-r-15 p-t-20 bg-white">
-            <div class="row push-10">
-                <div class="container">
-                    <div class="col-xs-12 text-center">
-                        <div class="col-md-4 col-md-offset-4 not-padding">
-                            <h2 style="margin: 0;">
-                                <b>Planning</b>
-                            </h2>
-                        </div>
+        <div class="row  p-l-15 p-r-15 p-t-20">
+            <a class="pull-right" href="{{ url('logout') }}">Cerrar sesion</a>
+        </div>
+        <div class="row push-10">
+            <div class="container">
+                <div class="col-xs-12 text-center">
+                    <div class="col-md-4 col-md-offset-4 not-padding">
+                        <h2 style="margin: 0;">
+                            <b>Planning</b>
+                        </h2>
                     </div>
                 </div>
             </div>
-            <div class="row push-20">
-                <div class="col-md-7">
-                    <button class="btn btn-success btn-tables btn-cons" type="button" data-type="checkin">
-                        <span class="bold">Check IN</span>
-                    </button>
+        </div>
+        <div class="row push-20">
+            <div class="col-md-7">
+                <button class="btn btn-success btn-tables btn-cons" type="button" data-type="checkin">
+                    <span class="bold">Check IN</span>
+                </button>
 
-                    <button class="btn btn-primary btn-tables btn-cons" type="button" data-type="checkout">
-                        <span class="bold">Check OUT</span>
-                    </button>
-                </div>
-                <div class="col-xs-12" id="resultSearchBook" style="display: none; padding-left: 0;"></div>
-                <div class="col-xs-12 content-tables" style="padding-left: 0;">
-                    @include('backend.planning._table', ['type'=> 'checkin'])
-                </div>
+                <button class="btn btn-primary btn-tables btn-cons" type="button" data-type="checkout">
+                    <span class="bold">Check OUT</span>
+                </button>
+            </div>
+            <div class="col-xs-12" id="resultSearchBook" style="display: none; padding-left: 0;"></div>
+            <div class="col-xs-12 content-tables" style="padding-left: 0;">
+                @include('backend.planning._table', ['type'=> 'checkin'])
             </div>
         </div>
-    <?php endif; ?>
+    </div>
 @endsection
 @section('scripts')
     <script src="/assets/js/notifications.js" type="text/javascript"></script>
