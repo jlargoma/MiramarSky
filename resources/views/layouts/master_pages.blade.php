1a4e89d21c64380d70c1d76ff26776d673222319
PinGoDev <pingodevweb@gmail.com>
2019-11-08 15:18:39 -0300

    DWIM 50 - Soporte Varios - remove GTags

--- resources/views/layouts/master_pages.blade.php
+++ resources/views/layouts/master_pages.blade.php
@@ -103,34 +103,7 @@
 
 
 		</style>
-		<script type="text/javascript">
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-66225892-1', 'auto');
-          ga('send', 'pageview');
-
-		</script>
-		<!-- Google Code for conversiones_lead Conversion Page -->
-		<script type="text/javascript">
-          /* <![CDATA[ */
-          var google_conversion_id = 834109020;
-          var google_conversion_language = "en";
-          var google_conversion_format = "3";
-          var google_conversion_color = "ffffff";
-          var google_conversion_label = "DHhZCN3wy3UQ3PzdjQM";
-          var google_remarketing_only = false;
-          /* ]]> */
-		</script>
-		<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-		</script>
-		<noscript>
-			<div style="display:inline;">
-				<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/834109020/?label=DHhZCN3wy3UQ3PzdjQM&amp;guid=ON&amp;script=0"/>
-			</div>
-		</noscript>
+		
 	</head>
 
 	<body class="stretched no-transition not-dark">
