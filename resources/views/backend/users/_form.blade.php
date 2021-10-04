6001eefb5de0f696e87e2f94c0b2e2f4ce73d1e4
Gerardo DWIM <pingodevweb@gmail.com>
2021-10-04 14:04:27 -0300

    change user edit

--- resources/views/backend/users/_form.blade.php
+++ resources/views/backend/users/_form.blade.php
@@ -16,7 +16,14 @@
       <label>Telefono</label>
       <input type="number" class="form-control" name="phone" placeholder="Telefono" required="" aria-required="true" aria-invalid="false" value="<?php echo $user->phone ?>">
     </div>
-   
+    <div class="col-field-2">
+      <label>DNI</label>
+      <input type="text" class="form-control" name="nif" placeholder="DNI" value="<?php echo $user->nif ?>">
+    </div>
+    <div class="col-field-2">
+      <label>Cta. Cte.</label>
+      <input type="text" class="form-control" name="iban" placeholder="Cta.Cte./IBAN" value="<?php echo $user->iban ?>">
+    </div>
     <div class="col-field-2">
       <label>Cargo</label>
       <select class="form-control full-width" name="role">
@@ -35,25 +42,18 @@
       <label>Contraseña</label>
       <input type="password" class="form-control" name="password"  aria-required="true" aria-invalid="false" value="" autocomplete="false">
     </div>
-   
+    <div class="col-field-2">
+      <label>Repetir Contraseña</label>
+      <input type="password" class="form-control" name="repassword"  aria-required="true" aria-invalid="false" value="" autocomplete="false">
+    </div>
     <div class="col-field-2">
       <label>Razon social</label>
       <input type="text" class="form-control" name="name_business" placeholder="Razon social" value="<?php echo $user->name_business ?>">
-
     </div>
-   
     <div class="col-field-2">
       <label>NIF/CIF/DNI/NIE</label>
       <input type="text" class="form-control" name="nif_business" placeholder="NIF/CIF/DNI/NIE" value="<?php echo $user->nif_business ?>">
-
     </div>
-   
-    <div class="col-field-2">
-      <label>Cta. Cte.</label>
-      <input type="text" class="form-control" name="iban" placeholder="Cta.Cte./IBAN" value="<?php echo $user->iban ?>">
-
-    </div>
-   
     <div class="col-field-2">
       <label>Dirección</label>
       <input type="text" class="form-control" name="address_business" placeholder="Dirección"value="<?php echo $user->address_business ?>">
